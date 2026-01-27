0c8f5efd4f7055c9de51a6f54cdfd2707f5c55a5
Pierre Nicolas <pierre.nicolas@localhost.localdomain>
2026-01-27 08:28:14 +0100

    2eme commit

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ let b = 45;
 
 b += a ;
 console.log("resultat de a + b = " + b);
+// on peut également déclarer et initialiser un objet de la façon suivante :
+
+// let aPerson= new Object();
+// aPerson.name = 'Jean';
+// aPerson.age = 35;
+// aPerson.gender = 'masculin';
+// aPerson.interest = ['musique', 'badminton'];
+let aPerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musique', 'Jeux videos'],
+};
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
